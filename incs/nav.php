11c2ce7833cf86bdeb9cd05f4f0aec0974c09d9f
Tuomas <tuomas@puppe.me>
2016-10-10 13:40:08 +0300

    nav #999

--- incs/nav.php
+++ incs/nav.php
@@ -2,6 +2,6 @@
 <nav class="navbar navbar-fixed-top vihreepohja" ng-controller="kello">
   <a class="navbar-brand vihreepohja" href="http://byts.fi/kopla">KOPLA {{ kello | date:'HH:mm:ss'}}</a>
   <form class="form-inline pull-xs-right" role="search" action="aikataulu.php" method="get">
-    <input class="form-control" type="tel" name="n" placeholder="Haku...">
+    <input class="form-control pull-xs-right" type="tel" name="n" placeholder="Haku...">
   </form>
 </nav>
