4731a6f8ad87a4b6c1b3d6c7ef55ca24275f0ca2
Tuomas <tuomas@puppe.me>
2016-10-10 18:14:56 +0300

    minors

--- incs/nav.php
+++ incs/nav.php
@@ -3,5 +3,6 @@
   <a class="navbar-brand vihreepohja" href="http://byts.fi/kopla">KOPLA {{ kello | date:'HH:mm:ss'}}</a>
   <form class="form-inline pull-xs-right" role="search" action="aikataulu.php" method="get">
     <input class="form-control pull-xs-right" type="tel" name="n" placeholder="Haku...">
+    <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
   </form>
 </nav>
