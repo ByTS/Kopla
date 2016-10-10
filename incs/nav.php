f573a18eeec24a17b08b76ff4fd85bbb432efa32
Tuomas <tuomas@puppe.me>
2016-10-10 13:38:49 +0300

    button remove

--- incs/nav.php
+++ incs/nav.php
@@ -2,7 +2,6 @@
 <nav class="navbar navbar-fixed-top vihreepohja" ng-controller="kello">
   <a class="navbar-brand vihreepohja" href="http://byts.fi/kopla">KOPLA {{ kello | date:'HH:mm:ss'}}</a>
   <form class="form-inline pull-xs-right" role="search" action="aikataulu.php" method="get">
-    <input class="form-control" type="number" name="n" placeholder="Haku...">
-    <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
+    <input class="form-control" type="tel" name="n" placeholder="Haku...">
   </form>
 </nav>
