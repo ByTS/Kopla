e9513e7ced63374539d2abf03fe5d36dbce5b1a3
puppee <pupp3@icloud.com>
2016-10-11 07:40:05 +0300

    Update nav.php
    Input field

--- incs/nav.php
+++ incs/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-fixed-top vihreepohja" ng-controller="kello">
 <a class="navbar-brand vihreepohja" href="http://byts.fi/kopla">KOPLA {{ kello | date:'HH:mm:ss'}}</a>
 <form class="form-inline pull-xs-right" role="search" action="aikataulu.php" method="get">
-<div class="input-group pull-xs-right">
+<div class="input-group pull-xs-right col-xs-4">
 <input type="tel" class="form-control" placeholder="Junanro" name="n">
 <span class="input-group-btn">
 <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
