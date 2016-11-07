a7e9a77bfce55db348d79d4561ed54ae3d08e638
Tuomas <tuomas@puppe.me>
2016-11-07 17:58:11 +0200

    station fix

--- asema.php
+++ asema.php
@@ -52,7 +52,7 @@ app.controller("asemat", function($scope, $http) {
 		</div>
 		<div class="container-fluid">
 			<span ng-controller="asemat">
-				<h2><span ng-hide="!(stations| filter:{'stationShortCode':'<?php echo $as;?>'}: true).length">Lähtevät, <span ng-repeat="x in stations | filter:{'stationShortCode':'<?php echo $as;?>'}: true | groupBy: !'scheduledTime'">{{x.stationName}} ({{x.stationShortCode}})</span></span>
+				<h2>Lähtevät, <span ng-repeat="x in stations | filter:{'stationShortCode'=='<?php echo $as;?>'}: true">{{x.stationName}} ({{x.stationShortCode}})</span>
 				</h2>
 			</span>
 			<table class="table table-striped table-sm" ng-controller="yhteydet" ng-hide="!rows.length">
