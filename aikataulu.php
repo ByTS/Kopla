f53e0b3d499cc451242c679708c2c9e13bfaf838
Tuomas <tuomas@puppe.me>
2016-11-10 14:29:10 +0200

    AE fix #2

--- aikataulu.php
+++ aikataulu.php
@@ -131,7 +131,7 @@
           </tr>
         </tbody>
       </table>
-	  <h6 ng-show="x.trainType==='AE'">Allegron toteumatiedot on saatavilla vain Suomen puolelta.</h6>
+	  <h5 ng-show="trainType=='AE'">Allegron toteumatiedot on saatavilla vain Suomen puolelta.</h5>
     </div>
   </div>
   <div class="col-md-6" ng-controller="liveTrains">
